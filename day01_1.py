db6ae01fbefe2c7bbeaf9dab63722564d751cc20
akyt2 <alfred.truong@gmail.com>
2023-01-03 14:31:41 +0800

    initial commit

---
+++ day01_1.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 22 10:51:21 2022
+
+@author: ahkar
+"""
+
+calories="""
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+
+calories
+
+input1=r'C:\Users\ahkar\OneDrive\Documents\GitHub\AoC 2022\00_1_input.txt'
+
+# read a file
+with open(input1,'r') as f:
+    lines = [line for line in f]
+
+lines
+input1
+
+
+ord('c')
+
+chr(999)
