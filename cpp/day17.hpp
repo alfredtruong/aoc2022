592bcc709f60507a521632ec2c85f2e1ae8f3833
akyt2 <alfred.truong@gmail.com>
2023-03-10 14:54:08 +0800

    mess around with bitmaps

--- cpp/day17.hpp
+++ cpp/day17.hpp
@@ -29,13 +29,16 @@ class Cave
 private:
   static const int WIDTH = 7;
   int m_rock_idx = 0;
+  int m_rock_bottom_left_idx = 2;
+  int m_rock_bottom_right_idx = 2;
+  bool m_rock_is_settled = false;
   std::list<std::bitset<WIDTH>> m_contents; // list as can pop levels in between
 
 public:
   Cave() {
-    add_rock_minus();
-
-    show_cave();
+    //add_rock_test();
+    blow_minus_test();
+    //blow_plus_test();
   }
 
   add_rock_test() {
@@ -60,25 +63,73 @@ public:
     m_contents.clear();
   }
 
-  void show_bitset(auto& bs)
+  blow_minus_test()
   {
-    for (int i=WIDTH-1;i>=0;i--)
+    bool verbose = true;
+    add_rock_minus();
+    show_cave();
+    blow_minus(true,verbose);
+    blow_minus(true,verbose);
+    blow_minus(true,verbose);
+    blow_minus(true,verbose);
+    blow_minus(true,verbose);
+    blow_minus(false,verbose);
+    blow_minus(false,verbose);
+    blow_minus(false,verbose);
+    blow_minus(false,verbose);
+    blow_minus(false,verbose);
+    blow_minus(false,verbose);
+    show_cave();
+  }
+
+  blow_plus_test()
+  {
+    bool verbose = true;
+    add_rock_plus();
+    show_cave();
+    blow_plus(true,verbose);
+    blow_plus(true,verbose);
+    blow_plus(true,verbose);
+    blow_plus(true,verbose);
+    blow_plus(true,verbose);
+    blow_plus(true,verbose);
+    blow_plus(false,verbose);
+    blow_plus(false,verbose);
+    blow_plus(false,verbose);
+    blow_plus(false,verbose);
+    blow_plus(false,verbose);
+    blow_plus(false,verbose);
+    show_cave();
+  }
+
+  void show_line(const auto& line,std::string blurb = "\n")
+  {
+    for (int i=0;i<WIDTH;i++)
     {
-      if (bs[i])
+      if (line[i])
         std::cout << '#';
       else
         std::cout << '.';
     }
+
+    std::cout << blurb;
   }
+
   void show_cave()
   {
-    std::cout << m_contents.size() << std::endl;
+    std::cout <<
+    "height = " << m_contents.size() <<
+    ", rock settled = " << m_rock_is_settled <<
+    ", rock bottom left = " << m_rock_bottom_left_idx <<
+    ", rock bottom right = " << m_rock_bottom_right_idx <<
+    std::endl;
 
     for (auto it=m_contents.rbegin();it!=m_contents.rend();it++)
     {
-      show_bitset(*it);
+      show_line(*it,"");
       std::cout << "\t" << (*it).any() << std::endl;
     }
+
   }
 
   void add_rock()
@@ -105,14 +156,22 @@ public:
 
   void add_rock_minus()
   {
+    m_rock_is_settled = false;
+    m_rock_bottom_left_idx = 2;
+    m_rock_bottom_right_idx = 5;
+
     m_contents.emplace_back(0b0);
     m_contents.emplace_back(0b0);
     m_contents.emplace_back(0b0);
-    m_contents.emplace_back(0b00011110);
+    m_contents.emplace_back(0b0111100);
   }
 
   void add_rock_plus()
   {
+    m_rock_is_settled = false;
+    m_rock_bottom_left_idx = 3;
+    m_rock_bottom_right_idx = 3;
+
     m_contents.emplace_back(0b0);
     m_contents.emplace_back(0b0);
     m_contents.emplace_back(0b0);
@@ -123,32 +182,219 @@ public:
 
   void add_rock_el()
   {
+    m_rock_is_settled = false;
+    m_rock_bottom_left_idx = 5;
+    m_rock_bottom_right_idx = 5;
+
     m_contents.emplace_back(0b0);
     m_contents.emplace_back(0b0);
     m_contents.emplace_back(0b0);
     m_contents.emplace_back(0b0011100);
-    m_contents.emplace_back(0b0000100);
-    m_contents.emplace_back(0b0000100);
+    m_contents.emplace_back(0b0010000);
+    m_contents.emplace_back(0b0010000);
   }
 
   void add_rock_pipe()
   {
+    m_rock_is_settled = false;
+    m_rock_bottom_left_idx = 2;
+    m_rock_bottom_right_idx = 2;
+
     m_contents.emplace_back(0b0);
     m_contents.emplace_back(0b0);
     m_contents.emplace_back(0b0);
-    m_contents.emplace_back(0b0010000);
-    m_contents.emplace_back(0b0010000);
-    m_contents.emplace_back(0b0010000);
-    m_contents.emplace_back(0b0010000);
+    m_contents.emplace_back(0b0000100);
+    m_contents.emplace_back(0b0000100);
+    m_contents.emplace_back(0b0000100);
+    m_contents.emplace_back(0b0000100);
   }
 
   void add_rock_block()
   {
+    m_rock_is_settled = false;
+    m_rock_bottom_left_idx = 2;
+    m_rock_bottom_right_idx = 3;
+
     m_contents.emplace_back(0b0);
     m_contents.emplace_back(0b0);
     m_contents.emplace_back(0b0);
-    m_contents.emplace_back(0b0011000);
-    m_contents.emplace_back(0b0011000);
+    m_contents.emplace_back(0b0001100);
+    m_contents.emplace_back(0b0001100);
+  }
+
+  bool is_line_empty(auto& line)
+  {
+      return line.any();
+  }
+
+  void blow(bool is_left)
+  {
+    switch (m_rock_idx)
+    {
+    case 0:
+      blow_minus(is_left);
+      break;
+    case 1:
+      blow_plus(is_left);
+      break;
+    case 2:
+      blow_el(is_left);
+      break;
+    case 3:
+      blow_pipe(is_left);
+      break;
+    case 4:
+      blow_block(is_left);
+      break;
+    }
+  }
+
+  void move_from_to(auto& line,int from_idx,int to_idx)
+  {
+    line[from_idx]=false;
+    line[to_idx]=true;
+  }
+  void blow_minus(bool is_left = false,bool verbose = false)
+  {
+    std::stringstream blurb;
+
+    // only care about top line
+    auto& line = m_contents.back();
+    int centre_offset = 0;
+    if (is_left)
+    {
+      // only move left if (1) not at edge and (2) not trapped by settled rock
+      if (m_rock_bottom_left_idx>centre_offset)
+      {
+        if (!line[m_rock_bottom_left_idx-1])
+        {
+          move_from_to(line,m_rock_bottom_right_idx,m_rock_bottom_left_idx-1);
+          m_rock_bottom_left_idx--;
+          m_rock_bottom_right_idx--;
+          if (verbose) blurb << " blow left" << std::endl;
+        }
+        else
+        {
+          if (verbose) blurb << " blocked by settled rock" << std::endl;
+        }
+      }
+      else
+      {
+        if (verbose) blurb << " already left-most" << std::endl;
+      }
+      if (verbose) show_line(line,blurb.str());
+    }
+    else
+    {
+      // only move right if (1) not at edge and (2) not trapped by settled rock
+      if (m_rock_bottom_right_idx<(WIDTH-1-centre_offset))
+      {
+        if (!line[m_rock_bottom_right_idx+1])
+        {
+          move_from_to(line,m_rock_bottom_left_idx,m_rock_bottom_right_idx+1);
+          m_rock_bottom_left_idx++;
+          m_rock_bottom_right_idx++;
+          if (verbose) blurb << " blow right" << std::endl;
+        }
+        else
+        {
+          if (verbose) blurb << " blocked by settled rock" << std::endl;
+        }
+      }
+      else
+      {
+        if (verbose) blurb << " already right-most" << std::endl;
+      }
+      if (verbose) show_line(line,blurb.str());
+    }
+  }
+
+  void blow_plus(bool is_left = false,bool verbose = false)
+  {
+    std::stringstream blurb;
+
+    auto iter = m_contents.end();
+    auto& line = *(--iter);
+    auto& top_line = *(--iter);
+    auto& middle_line = *(--iter);
+    auto& bottom_line = *(--iter);
+
+    show_line(top_line);
+    show_line(middle_line);
+    show_line(bottom_line);
+    // care about 3 lines
+
+    int centre_offset = 1;
+
+    if (is_left)
+    {
+      // only move left if (1) not at edge and (2) not trapped by settled rock
+      if (m_rock_bottom_left_idx>centre_offset)
+      {
+        if (!line[m_rock_bottom_left_idx-1])
+        {
+          top_line >>= 1;
+          middle_line;
+          bottom_line >>= 1;
+          m_rock_bottom_left_idx--;
+          m_rock_bottom_right_idx--;
+          if (verbose) blurb << " blow left" << std::endl;
+        }
+        else
+        {
+          if (verbose) blurb << " blocked by settled rock" << std::endl;
+        }
+      }
+      else
+      {
+        if (verbose) blurb << " already left-most" << std::endl;
+      }
+      if (verbose) show_line(line,blurb.str());
+    }
+    else
+    {
+      // only move right if (1) not at edge and (2) not trapped by settled rock
+      if (m_rock_bottom_right_idx<(WIDTH-1-centre_offset))
+      {
+        if (!line[m_rock_bottom_right_idx+1])
+        {
+          //line <<= 1;
+          m_rock_bottom_right_idx++;
+          line[m_rock_bottom_right_idx]=true;
+          line[m_rock_bottom_left_idx]=false;
+          m_rock_bottom_left_idx++;
+          if (verbose) blurb << " blow right" << std::endl;
+        }
+        else
+        {
+          if (verbose) blurb << " blocked by settled rock" << std::endl;
+        }
+      }
+      else
+      {
+        if (verbose) blurb << " already right-most" << std::endl;
+      }
+      if (verbose) show_line(line,blurb.str());
+    }
+
+
+
+  }
+
+
+  void blow_el(bool is_left = false)
+  {
+
+  }
+
+  void blow_pipe(bool is_left = false)
+  {
+
+  }
+
+  void blow_block(bool is_left = false)
+  {
+
   }
 
   void drop_rock()
